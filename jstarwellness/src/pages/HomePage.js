20f1677e4cc2834b3b4741d45af9ae78104b905f
JOATZ <136692977+JOATZ@users.noreply.github.com>
2024-02-22 14:57:13 +0200

    home page intor and header done

--- jstarwellness/src/pages/HomePage.js
+++ jstarwellness/src/pages/HomePage.js
@@ -8,6 +8,20 @@ const HomePage = () => {
     return (
         <>
             <HomeCarousel />
+            <h1 className='home-page-header'>Rediscover Your Confidence!</h1>
+            <h2 className='home-page-intro'>
+                Embark on a journey of holistic wellness with our comprehensive
+                services. With over two decades of experience in the wellness
+                industry, our personalized nutrition and exercise coaching are
+                tailored to your unique needs. Through Hair Tissue Mineral
+                Analysis Testing, gain insights into your body's balance and
+                well-being. Experience the transformative artistry of
+                paramedical procedures, fostering confidence and enhancing
+                natural beauty. With compassionate care and personalized
+                consultations, we are committed to empowering you to embrace
+                your season, rediscover your confidence, and achieve holistic
+                well-being.
+            </h2>
             <Container className='home-page-buttons'>
                 <Button
                     tag='a'
