300fdafb39c5bf35fbe0a2ca6d74ba7de7f3e9e1
JOATZ <136692977+JOATZ@users.noreply.github.com>
2024-04-22 14:02:45 +0300

    set ,menu open false onClick, re-incoporated top panel into header

--- jstarwellness/src/components/Header/Header.js
+++ jstarwellness/src/components/Header/Header.js
@@ -8,28 +8,79 @@ import {
     FaInfoCircle,
     FaList
 } from 'react-icons/fa'
+import {
+    FaEnvelope,
+    FaFacebookSquare,
+    FaInstagramSquare,
+    FaPhoneAlt,
+    FaTwitterSquare
+} from 'react-icons/fa'
 import { NavLink } from 'react-router-dom'
 import {
+    Col,
     Collapse,
     Container,
     Nav,
     Navbar,
     NavbarBrand,
     NavbarToggler,
-    NavItem
+    NavItem,
+    Row
 } from 'reactstrap'
 
-import TopPanel from './TopPanel'
-
 import 'components/Header/Header.css'
-/*MOVE TOGGLER! and set bg color*/
+
 const Header = () => {
     const [menuOpen, setMenuOpen] = useState(false)
 
     return (
         <>
             <Container fluid className='top-panel-container d-none d-lg-flex'>
-                <TopPanel />
+                <Row className='top-panel-contents'>
+                    <Col className='top-panel'>
+                        <a
+                            role='button'
+                            className='btn btn-link'
+                            href='tel:+18052687565'
+                        >
+                            <FaPhoneAlt /> 1-805-268-7565
+                        </a>
+                        |
+                        <a
+                            role='button'
+                            className='btn btn-link'
+                            href='mailto:Stepup2shapeup@gmail.com'
+                        >
+                            <FaEnvelope /> Stepup2shapeup@gmail.com
+                        </a>
+                    </Col>
+                    <Col className='top-panel text-end'>
+                        <a
+                            href='https://facebook.com'
+                            target='_blank'
+                            rel='noopener noreferrer'
+                            className='me-2'
+                        >
+                            <FaFacebookSquare />
+                        </a>
+                        <a
+                            href='http://www.instagram.com/gotyoucovered805'
+                            target='_blank'
+                            rel='noopener noreferrer'
+                            className='me-2'
+                        >
+                            <FaInstagramSquare />
+                        </a>
+                        <a
+                            href='https://twitter.com'
+                            target='_blank'
+                            rel='noopener noreferrer'
+                            className='me-2'
+                        >
+                            <FaTwitterSquare />
+                        </a>
+                    </Col>
+                </Row>
             </Container>
             <Navbar dark sticky='top' expand='lg'>
                 <NavbarBrand href='/' className='ms-5'>
@@ -46,34 +97,58 @@ const Header = () => {
                 <Collapse isOpen={menuOpen} navbar>
                     <Nav className='ms-auto' navbar>
                         <NavItem>
-                            <NavLink className='nav-link' to='/'>
+                            <NavLink
+                                className='nav-link'
+                                to='/'
+                                onClick={() => setMenuOpen(false)}
+                            >
                                 <FaHome />
                                 Home
                             </NavLink>
                         </NavItem>
                         <NavItem>
-                            <NavLink className='nav-link' to='/services'>
+                            <NavLink
+                                className='nav-link'
+                                to='/services'
+                                onClick={() => setMenuOpen(false)}
+                            >
                                 <FaList /> Services
                             </NavLink>
                         </NavItem>
                         <NavItem>
-                            <NavLink className='nav-link' to='/gallery'>
+                            <NavLink
+                                className='nav-link'
+                                to='/gallery'
+                                onClick={() => setMenuOpen(false)}
+                            >
                                 <FaCamera /> Gallery
                             </NavLink>
                         </NavItem>
                         <NavItem>
-                            <NavLink className='nav-link' to='/testimonials'>
+                            <NavLink
+                                className='nav-link'
+                                to='/testimonials'
+                                onClick={() => setMenuOpen(false)}
+                            >
                                 <BsFillPatchExclamationFill />
                                 Testimonials
                             </NavLink>
                         </NavItem>
                         <NavItem>
-                            <NavLink className='nav-link' to='/about'>
+                            <NavLink
+                                className='nav-link'
+                                to='/about'
+                                onClick={() => setMenuOpen(false)}
+                            >
                                 <FaInfoCircle /> About
                             </NavLink>
                         </NavItem>
                         <NavItem>
-                            <NavLink className='nav-link' to='/contact'>
+                            <NavLink
+                                className='nav-link'
+                                to='/contact'
+                                onClick={() => setMenuOpen(false)}
+                            >
                                 <FaAddressCard />
                                 Contact
                             </NavLink>
