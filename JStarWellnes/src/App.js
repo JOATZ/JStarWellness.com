d0bc74f70e7f4a4d552c0938a0f13d9afd72d6c9
JOATZ <136692977+JOATZ@users.noreply.github.com>
2024-04-23 16:06:44 +0300

    installed webpack anlayzer for CRA

--- JStarWellnes/src/App.js
+++ JStarWellnes/src/App.js
@@ -1,12 +1,7 @@
+import React, { lazy, Suspense } from 'react'
 import { store } from 'app/store'
 import Footer from 'components/Footer/Footer'
 import Header from 'components/Header/Header'
-import AboutPage from 'pages/About'
-import BeforeAndAfterNT from 'pages/BeforeAndAfterNT'
-import BeforeAndAfterPM from 'pages/BeforeAndAfterPM'
-import ContactPage from 'pages/ContactPage'
-import EducationPage from 'pages/Education/EducationPage'
-import Gallery from 'pages/Gallery'
 import HomePage from 'pages/HomePage'
 import ServicePage from 'pages/ServicePage'
 import Testimonials from 'pages/Testimonials/Testimonials'
@@ -14,6 +9,12 @@ import { Provider } from 'react-redux'
 import { Route, Routes } from 'react-router-dom'
 
 import 'App.css'
+const AboutPage = lazy(() => import('pages/About'))
+const BeforeAndAfterNT = lazy(() => import('pages/BeforeAndAfterNT'))
+const BeforeAndAfterPM = lazy(() => import('pages/BeforeAndAfterPM'))
+const ContactPage = lazy(() => import('pages/ContactPage'))
+const EducationPage = lazy(() => import('pages/Education/EducationPage'))
+const Gallery = lazy(() => import('pages/Gallery'))
 
 function App() {
     return (
@@ -21,23 +22,31 @@ function App() {
             <div className='App'>
                 <Header />
                 <div className='app-content'>
-                    <Routes>
-                        <Route path='/' element={<HomePage />} />
-                        <Route path='Services' element={<ServicePage />} />
-                        <Route path='Education' element={<EducationPage />} />
-                        <Route path='Contact' element={<ContactPage />} />
-                        <Route path='About' element={<AboutPage />} />
-                        <Route path='Gallery' element={<Gallery />} />
-                        <Route path='Testimonials' element={<Testimonials />} />
-                        <Route
-                            path='BeforeAndAfterNT'
-                            element={<BeforeAndAfterNT />}
-                        />
-                        <Route
-                            path='BeforeAndAfterPM'
-                            element={<BeforeAndAfterPM />}
-                        />
-                    </Routes>
+                    <Suspense fallback={<div>Loading...</div>}>
+                        <Routes>
+                            <Route path='/' element={<HomePage />} />
+                            <Route path='Services' element={<ServicePage />} />
+                            <Route
+                                path='Education'
+                                element={<EducationPage />}
+                            />
+                            <Route path='Contact' element={<ContactPage />} />
+                            <Route path='About' element={<AboutPage />} />
+                            <Route path='Gallery' element={<Gallery />} />
+                            <Route
+                                path='Testimonials'
+                                element={<Testimonials />}
+                            />
+                            <Route
+                                path='BeforeAndAfterNT'
+                                element={<BeforeAndAfterNT />}
+                            />
+                            <Route
+                                path='BeforeAndAfterPM'
+                                element={<BeforeAndAfterPM />}
+                            />
+                        </Routes>
+                    </Suspense>
                 </div>
                 <Footer />
             </div>
